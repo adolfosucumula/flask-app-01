02b33d275ee0912d67f2160cc8a47793de015f4a
adolfosucumula <rocketmc2009@gmail.com>
2024-02-07 08:34:16 +0000

    Store the user information since their Id is stored  in the session

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,6 +10,9 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
+# ============= USER REGISTRACTION FUNCTION ==================
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -38,3 +41,48 @@ def register():
         flash(error)
         
     return render_template('auth/register.html')
+
+
+    # ======================= USER LOGGIN FUNCTION =====================
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if  user  in None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+# MAKING AVAILABLE THE USER INFORMATIONS SINCE THEIR ID IS STORED IN THE SESSION
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session. get('user_id')
+
+    if user_id is None:
+        g .user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE  id = ?', (user_id,)
+        ).fetchone()
+
+
+        
